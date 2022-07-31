1628303d3a5d198c0f24f1e6796f8fbb1f16e154
Alison <alisonal1@hotmail.com>
2022-07-31 15:34:33 -0300

    State: Add situacao de desconto extra no orcamento

--- loja/src/orcamento/Orcamento.java
+++ loja/src/orcamento/Orcamento.java
@@ -7,10 +7,31 @@ public class Orcamento {
 
     private BigDecimal valor;
     private int quantidadeItens;
+    private String situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+    }
+
+    //PROBLEMA: nova situacao de desconto (Desconto Extra)
+    //caso o cliente peça mais desconto como vamos fazer?.
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDescontoExtra = BigDecimal.ONE;
+
+        if (situacao.equals("EM ANALISE")) {
+            valorDescontoExtra = new BigDecimal("0.05");
+        }
+        if (situacao.equals("APROVADO")) {
+            valorDescontoExtra = new BigDecimal("0.05");
+        }
+
+        this.valor = this.valor.subtract(valorDescontoExtra);
+    }
+
+    public void aprovar(){
+        //situacoes tem um anterior/proximo.
+        this.situacao= "APROVADO";
     }
 
     public BigDecimal getValor() {
